5da55fa13c5e6cb93466f32843c02e389890c7de
Mathias Leding <mledin14@student.aau.dk>
2015-10-19 13:47:33 +0200

    Ty

--- P3/P3/Viewmodels/ViewmodelBAse.cs
+++ P3/P3/Viewmodels/ViewmodelBAse.cs
@@ -42,10 +42,10 @@ namespace P3.Viewmodels
         internal bool toggleFullScreen;
         public bool ToggleFullScreen
         {
-            get { return this.ToggleFullScreen; }
+            get { return this.toggleFullScreen; }
             set
             {
-                this.ToggleFullScreen = value;
+                this.toggleFullScreen = value;
                 RaisePropertyChanged();
             }
         }
