3c1facb1e8759d1900491dffc59617ddf6a911a8
RefotsirkNged <kristoffer@hylleby.dk>
2015-10-19 12:41:18 +0200

    stuf

--- P3/P3/Viewmodels/DummyViewModel.cs
+++ P3/P3/Viewmodels/DummyViewModel.cs
@@ -13,11 +13,45 @@ namespace P3.Viewmodels
 {
     class DummyViewModel : ViewModelBase
     {
-        string _Age;
-        public string Age
+        private int _Age;
+        public int Age
         {
             get { return _Age; }
             set { _Age = value; RaisePropertyChanged(); }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        private AccessLevel _userAccessLevel;
+        public AccessLevel UserAccessLevel
+        {
+            get { return _userAccessLevel; }
+            set
+            {
+                if (_userAccessLevel != value)
+                {
+                    _userAccessLevel = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public DummyViewModel()
+        {
+            Age = 20;
+            Name = "gert";
+            UserAccessLevel = 0;
         }
     }
 }
