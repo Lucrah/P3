63c1fcbfa646c6d21c2adc420d678ac3db47ef6d
BogiThomsen <bogi1511@gmail.com>
2015-10-19 14:44:10 +0200

    Kode smidt ind i det andet ting noget.

---
+++ P3/P3/Models/House.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  class House : Listing
+  {
+     public House(string streetName, int houseNumber, int areaCode, string city) : base(streetName, houseNumber, areaCode, city)
+    {
+      
+    }
+
+    public House(string streetName, int houseNumber, int areaCode, string city,  int priceOfHouse, int sizeOfHouse, int numberOfRooms, 
+                 int numberOfBathrooms, int sizeOfProperty, int sizeOfBasement, int numberOfFloors, int yearBuilt) : base(streetName, 
+                 houseNumber, areaCode, city, priceOfHouse, sizeOfHouse, yearBuilt) 
+    {
+      NumberOfRooms = numberOfRooms;
+      NumberOfBathrooms = numberOfBathrooms;
+      SizeOfProperty = sizeOfProperty;
+      SizeOfBasement = sizeOfBasement;
+      NumberOfFloors = numberOfFloors;
+    }
+
+    private int _numberOfRooms;
+
+    public int NumberOfRooms
+    {
+      get { return _numberOfRooms; }
+      private set { _numberOfRooms = value; }
+    }
+
+    private int _numberOfBathrooms;
+
+    public int NumberOfBathrooms
+    {
+      get { return _numberOfBathrooms; }
+      private set { _numberOfBathrooms = value; }
+    }
+
+    private int _sizeOfProperty;
+
+    public int SizeOfProperty
+    {
+      get { return _sizeOfProperty; }
+      private set { _sizeOfProperty = value; }
+    }
+
+    private int _sizeOfBasement;
+
+    public int SizeOfBasement
+    {
+      get { return _sizeOfBasement; }
+      private set { _sizeOfBasement = value; }
+    }
+
+    private int _numberOfFloors;
+
+    public int NumberOfFloors
+    {
+      get { return _numberOfFloors; }
+      set { _numberOfFloors = value; }
+    }
+
+    /*private bool _andelsbolig;
+
+    public bool AndelsBolig
+    {
+      get { return _andelsbolig; }
+      private set { _andelsbolig = value; }
+    }
+    private bool _fritidsBolig;    //Brug muligvis til checkbokse omkring andelsbolig/fritidsbolig/almindeligt hus.
+
+    public bool FritidsBolig
+    {
+      get { return _fritidsBolig; }
+      private set { _fritidsBolig = value; }
+    }*/
+
+    
+    
+
+
+
+  }
+}
---
+++ P3/P3/Models/Listing.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  class Listing 
+  {
+    public Listing(string streetName, int houseNumber, int areaCode, string city)
+    {
+      StreetName = streetName;
+      AreaCode = areaCode;
+      Address = streetName + " " + Convert.ToString(houseNumber) + ", " + Convert.ToString(areaCode) + " " + city; 
+      AddressForURL = streetName + "+" + Convert.ToString(houseNumber) + ",+" + Convert.ToString(areaCode) + "+" + city;
+    }
+    public Listing(string streetName, int houseNumber, int areaCode, string city,  int priceOfHouse, int sizeOfHouse, int yearBuilt)
+    {
+      StreetName = streetName;
+      AreaCode = areaCode;
+      Address = streetName + " " + Convert.ToString(houseNumber) + ", " + Convert.ToString(areaCode) + " " + city; 
+      AddressForURL = streetName + "+" + Convert.ToString(houseNumber) + ",+" + Convert.ToString(areaCode) + "+" + city; //used for GeoCode lookups.
+      Price = priceOfHouse;
+      Size = sizeOfHouse;
+      YearBuilt = yearBuilt;
+    }
+    #region property
+    private int _areaCode;
+
+    public int AreaCode
+    {
+      get { return _areaCode; }
+      set { _areaCode = value; }
+    }
+
+    
+    private string _streetName;
+
+    public string StreetName
+    {
+      get { return _streetName; }
+      set { _streetName = value; }
+    }
+
+    private string _address;
+
+    public string Address
+    {
+      get { return _address; }
+      private set { _address = value; }
+    }
+
+    private string _addressForURL;
+
+    public string AddressForURL
+    {
+      get { return _addressForURL; }
+      private set { _addressForURL = value; }
+    }
+    
+    private int _price;
+
+    public int Price
+    {
+      get { return _price; }
+      private set { _price = value; }
+    }
+    
+    private int _size;
+
+    public int Size
+    {
+      get { return _size; }
+      private set { _size = value; }
+    }
+
+    private int _yearBuilt;
+
+    public int YearBuilt
+    {
+      get { return _yearBuilt; }
+      set { _yearBuilt = value; }
+    }
+
+    public Coordinates coordinates = new Coordinates();
+#endregion 
+
+
+    public void GetGeoCode(double[] a)
+    {
+      coordinates.lat = a[0];
+      coordinates.lng = a[1];
+    }
+
+  }
+}
