597adfd73b61fdf2d58db3b032ffd052c2896a44
RefotsirkNged <kristoffer@hylleby.dk>
2015-10-19 11:57:31 +0200

    stuf

---
+++ P3/P3/Models/BaseINPCModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace P3.Models
+{
+    class BaseINPCModel : INotifyPropertyChanged
+    {
+        #region INPC
+        //This part needs to be implemented in all models, and viewmodels. Viewmodels inherit it from viewmodelbase though.
+        internal void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
