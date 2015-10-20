818d0ed93a8a40836f3db42a6e689e638129584b
RefotsirkNged <kristoffer@hylleby.dk>
2015-10-20 13:33:49 +0200

    databae? maybae

---
+++ P3/P3/Data/dbo.Table.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	    [StreetName]     VARCHAR (50) NULL,
+    [HouseNumber]    INT          NULL,
+    [AreaCode]       INT          NULL,
+    [City]           VARCHAR (50) NULL,
+    [Type]           VARCHAR (50) NULL,
+    [Latitude]       INT          NULL,
+    [Longitude]      INT          NULL,
+    [Price]          INT          NULL,
+    [Size]           INT          NULL,
+    [Rooms]          INT          NULL,
+    [Bathrooms]      INT          NULL,
+    [SizeOfProperty] INT          NULL,
+    [Basement]       INT          NULL,
+    [Floors]         INT          NULL,
+    [Sold]           DATETIME     NULL,
+    [YearBuilt]      INT          NULL
+)
