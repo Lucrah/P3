eda410894ef4d4ee76a1ce92e0d9b5f1a52212e0
jaadare <jaadare88@gmail.com>
2015-10-26 11:24:50 +0100

    import

--- P3/P3/Helpers/Import.cs
+++ P3/P3/Helpers/Import.cs
@@ -20,9 +20,9 @@ namespace P3.Helpers
             rel_path = rel_path.Replace(dir_info, "");
             savefile = rel_path.Remove(rel_path.Length - 1) + @"Data\";
         }
-        private void import_forsale()
+        private void importForSale()
         {
-            var reader = new StreamReader(File.OpenRead(savefile), Encoding.Default);
+            var reader = new StreamReader(File.OpenRead(savefile + "til salg 20th Oct 00_33.csv"), Encoding.Default);
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
@@ -35,9 +35,9 @@ namespace P3.Helpers
                 string streetName = String.Empty;
                 string houseNumber = String.Empty;
                 Match regexMatch = Regex.Match(adresse, "\\d");
-                if (regexMatch.Success) //Found numeric part in the coverage string
+                if (regexMatch.Success)
                 {
-                    int digitStartIndex = regexMatch.Index; //Get the index where the numeric digit found
+                    int digitStartIndex = regexMatch.Index;
                     streetName = adresse.Substring(0, digitStartIndex);
                     houseNumber = adresse.Substring(digitStartIndex);
                 }
@@ -46,15 +46,42 @@ namespace P3.Helpers
                 int sizeProperty = Convert.ToInt32(Regex.Replace(values[5], "[^0-9.]", ""));
                 int rooms = Convert.ToInt32(values[5]);
                 int areaCode = Convert.ToInt32(values[6]);
-
-                Listing(streetName, houseNumber, areaCode,  city, priceOfHouse,  sizeOfHouse, yearBuilt)
-
-
-
-
-                }
+            }
 
         }
+        private void importSold()
+        {
+            var reader = new StreamReader(File.OpenRead(savefile + "Solgte.csv"), Encoding.Default);
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                string[] values = line.Split(',');
+
+                int room = Convert.ToInt32(values[0]);
+                string salesType = values[1];
+                int sizeHouse = Convert.ToInt32(values[2]);
+                string uncleanSqrPrice = values[3];
+                int cleansqrprice = Convert.ToInt32(uncleanSqrPrice.Replace(".", ""));
+                int yearBuilt = Convert.ToInt32(values[4]);
+                string uncleanPrice = values[5];
+                int cleanPrice = Convert.ToInt32(uncleanPrice.Replace(".", ""));
+                string adresse = values[6];
+                string streetName = String.Empty;
+                string houseNumber = String.Empty;
+                Match regexMatch = Regex.Match(adresse, "\\d");
+                if (regexMatch.Success)
+                {
+                    int digitStartIndex = regexMatch.Index;
+                    streetName = adresse.Substring(0, digitStartIndex);
+                    houseNumber = adresse.Substring(digitStartIndex);
+                }
+                string propertyType = values[7];
+                int areaCode = Convert.ToInt32(values[8]);
+
+
+            }
+        }
+
 
     }
 }
