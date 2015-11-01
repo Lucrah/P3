5ea186b3a8887c146dfeadabc9be8bb49131da02
RefotsirkNged <kristoffer@hylleby.dk>
2015-11-01 22:20:50 +0100

    shit

--- P3/P3/Viewmodels/SearchScreenViewModel.cs
+++ P3/P3/Viewmodels/SearchScreenViewModel.cs
@@ -9,5 +9,33 @@ namespace P3.ViewModels
 {
     class SearchScreenViewModel : Screen
     {
+        #region SearchWindowProperties
+
+        private double _priceSliderValue;
+        private double _areaSliderValue;
+
+        #endregion
+        #region SearchWindowPublicProperties
+        public double PriceSliderValue
+        {
+            get { return _priceSliderValue; }
+            set
+            {
+                _priceSliderValue = value;
+                NotifyOfPropertyChange(() => PriceSliderValue);
+            }
+        }
+
+        public double AreaSliderValue
+        {
+            get { return _areaSliderValue; }
+            set
+            {
+                _areaSliderValue = value;
+                NotifyOfPropertyChange(() => AreaSliderValue);
+            }
+        }
+
+        #endregion
     }
 }
