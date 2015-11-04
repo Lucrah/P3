4b5e6655c02772937ac03b33de7d189423376be0
RefotsirkNged <kristoffer@hylleby.dk>
2015-11-04 18:05:22 +0100

    using mahapps.metro wuu

---
+++ P3/i hate windows/Models/Listings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using P3.Helpers;
+
+namespace P3.Models
+{
+  class Listings
+  {
+    static Dictionary<string, Listing> listingsDB = new Dictionary<string, Listing>();
+    public Listings()
+    {
+      
+    }
+
+    public void importListings()
+    {
+      //importering fra DB
+      // >>>>Lav funktion der finder ud af hvilken type ejendom det er og opretter instants af den rigtige klasse<<<<
+            //kald samme funktion i searchForProperty
+      //Tilføj til listen  (listingsDB.Add();) // eller find ud af hvordan man henter/smider data i databasen
+      //Hent coordinater til alle listings.
+      //Kun kør getCoords hvis de ikke haves ---- if coordinates x/y == 0, then lookup. (Funktionality.getCoordinates(listing);)
+      
+    } 
+
+         
+    //skal måske flyttes til functionality
+    public Dictionary<string, Listing> searchForProperty(double distance, string streetName, int houseNumber, int areaCode, string city, Dictionary<string, Listing> listingDB)
+    {
+      string address = streetName + " " + Convert.ToString(houseNumber) + ", " + Convert.ToString(areaCode) + " " + city;
+      Dictionary<string, Listing> result = new Dictionary<string, Listing>();
+      if (!listingDB.ContainsKey(address))
+      {
+        //new Listing something ... funktion som defineret i importListings();
+        Listing something = new Listing(streetName, houseNumber, areaCode, city);
+        Funktionality.getCoordinates(something);
+        //tilføj til DB
+      }
+
+      foreach (KeyValuePair<string, Listing> listing in listingDB)
+      {
+        if (distance >= Funktionality.convertToDistance(listing.Value, listingDB[address]))
+        {
+          result.Add(listing.Key, listing.Value);
+        }
+      }
+
+      return result;
+    }
+
+
+
+  }
+}
---
+++ P3/i hate windows/ViewModels/ShellViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using P3.Interfaces;
+
+namespace P3.ViewModels
+{
+    [Export(typeof (IShell))]
+    class ShellViewModel : Conductor<object>.Collection.OneActive, IShell
+    {
+        #region IOC Fields
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IWindowManager _windowManager;
+        #endregion
+
+        [ImportingConstructor]
+        public ShellViewModel()
+        {
+            _windowManager = IoC.Get<IWindowManager>();
+            _eventAggregator = IoC.Get<IEventAggregator>();
+            WindowTitle = "MæglerHelper";
+            ActivateItem(new SearchScreenViewModel());
+        }
+
+        #region Fields / getset
+
+        private string _windowTitle;
+
+        public string WindowTitle
+        {
+            get
+            {
+                return _windowTitle;
+            }
+
+            set
+            {
+                _windowTitle = value;
+                NotifyOfPropertyChange(() => WindowTitle);
+            }
+        }
+
+        #endregion
+
+        #region NavigateFunctions
+        /*Shows how to display an item(in this case a usercontrol)
+         *you then do something like this on a button:
+         * <Button x:Name="ShowResultScreen" Content="ResultScreen" Height="29.5" VerticalAlignment="Top" Width="96"/>
+         * And really, this can be used to bind any function to any button/other control.
+         * This also shows the aforementioned ActivateItem. If i wanted to pass settings or something into the views it could be done here.
+         * */
+        public void ShowGraphScreen()
+        {
+            ActivateItem(new GraphScreenViewModel());
+        }
+        public void ShowPropertyInfo()
+        {
+            ActivateItem(new PropertyInfoViewModel());
+        }
+        public void ShowResultScreen()
+        {
+            ActivateItem(new ResultScreenViewModel());
+        }
+        public void ShowSearchScreen()
+        {
+            ActivateItem(new SearchScreenViewModel());
+        }
+        #endregion
+    }
+}
