f4bcd256c35dd0707edc7c4a1492d8d9eb3ec643
BogiThomsen <bogi1511@gmail.com>
2015-11-05 14:09:08 +0100

    fuck latl/lat

--- Bogi/Classes and some Functionality/ConsoleApplication1/Funktionality.cs
+++ Bogi/Classes and some Functionality/ConsoleApplication1/Funktionality.cs
@@ -150,7 +150,7 @@ namespace ConsoleApplication1
           using (StreamWriter tw = new StreamWriter(savefile + "ImportedProperties_Bogi1.csv", true))
           {
             tw.WriteLine("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11};{12}", property.ID, property.PropertyType, property.StreetName, property.HouseNumber, property.AreaCode, 
-              property.YearBuilt, property.Rooms,property.Price, property.Sqrprice, property.SalesDate, property.SalesType, property.Lng, property.Lng);
+              property.YearBuilt, property.Rooms,property.Price, property.Sqrprice, property.SalesDate, property.SalesType, property.Lat, property.Lng);
           }
 
         }
