d2cc06e224d13b2d5526c41611d79e1a316dd606
Mathias Leding <mledin14@student.aau.dk>
2015-12-07 16:32:48 +0100

    Load-delen

--- P3/i hate windows/ViewModels/SearchScreenViewModel.cs
+++ P3/i hate windows/ViewModels/SearchScreenViewModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
 using P3.Models;
-using P3.ViewModels;
+
 using MySql.Data.MySqlClient;
 using System.ComponentModel.Composition;
 using P3.Helpers;
@@ -112,7 +112,7 @@ namespace P3.ViewModels
             ResultScreen = new ResultScreenViewModel(ResultsReturned, SearchSettings, _windowManager, _eventAggregator);
 
             //Jeppes funktioner, responsible for saving the searchsettings to a .csv so you can view recent searches.
-            SaveSearchSettings();
+            SearchHistoryCreater();
         }
 
         public void Print()
@@ -180,5 +180,130 @@ namespace P3.ViewModels
                 NotifyOfPropertyChange(() => IsPrintOpen);
             }
         }
+
+    public void SearchHistoryCreater()
+    {
+      string history = GetPath() + @"/History";
+
+
+      if (!Directory.Exists(history))
+      {
+        Directory.CreateDirectory(history);
+      }
+
+      bool freeSpace = true;
+      for (int i = 1; i <= 10; i++)
+      {
+        string historyFile = GetPath() + @"/History/" + i + @".csv";
+
+        if (!File.Exists(historyFile))
+        {
+          WriteToFile(historyFile);
+          freeSpace = true;
+          break;
+        }
+        freeSpace = false;
+      }
+
+      if (!freeSpace)
+      {
+        File.Delete(GetPath() + @"/History/1.csv");
+        string dir = GetPath() + @"/History/";
+        for (int i = 1; i < 10; i++)
+        {
+          string oldFile = dir + (i + 1) + @".csv";
+          string newFile = dir + i + @".csv";
+          File.Move(oldFile, newFile);
+        }
+
+        SearchHistoryCreater();
+      }
     }
+
+    public void WriteToFile(string historyFile)
+    {
+      
+      using (StreamWriter writer = File.CreateText(historyFile))
+      {
+        writer.WriteLine(SearchSettings.SearchInput ?? "");
+
+        //Hustype laves når det er lavet om :))
+
+        writer.WriteLine(Convert.ToString(SearchSettings.PriceSliderLowerValue) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.PriceSliderHigherValue) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.SizeSliderLowerValue) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.SizeSliderHigherValue) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.DowntimeLowerValue) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.DowntimeHigherValue) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.AreaSliderLowerValue) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.AreaSliderHigherValue) ?? "");
+
+        writer.WriteLine(Convert.ToString(SearchSettings.MinPrKvm) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.MaxPrKvm) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.MinGroundSize) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.MaxGroundSize) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.MinYearBuilt) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.MaxYearBuilt) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.MinRoomCount) ?? "");
+        writer.WriteLine(Convert.ToString(SearchSettings.MaxRoomCount) ?? "");
+
+        writer.WriteLine(Convert.ToString(SearchSettings.Sold));
+        writer.WriteLine(Convert.ToString(SearchSettings.ForSale));
+        writer.WriteLine(Convert.ToString(SearchSettings.SameRoad));
+        writer.WriteLine(Convert.ToString(SearchSettings.SameZipCode));
+      }
+    }
+
+    public BindableCollection<string> ScanDirectory()
+    {
+      string history = GetPath() + @"/History";
+      BindableCollection<string> filesFound = new BindableCollection<string>(Directory.GetFiles(history));
+
+      for (int i = 0; i < filesFound.Count; i++)
+      {
+        filesFound[i] = filesFound[i].Replace(Directory.GetCurrentDirectory(), "");
+        filesFound[i] = new DirectoryInfo(filesFound[i]).Name;
+        filesFound[i] = filesFound[i].Replace(".csv", "");
+      }
+
+      return filesFound;
+    }
+
+    public void LoadFromFile()
+    {
+      string history = GetPath() + @"/History";
+      using (StreamReader reader = File.OpenText(history))
+      {
+        SearchSettings.SearchInput = reader.ReadLine() ?? "";
+
+        //Hustype laves når det lort er done
+
+        SearchSettings.PriceSliderLowerValue = Convert.ToDouble(reader.ReadLine() ?? "");
+        SearchSettings.PriceSliderHigherValue = Convert.ToDouble(reader.ReadLine() ?? "");
+        SearchSettings.SizeSliderLowerValue = Convert.ToDouble(reader.ReadLine() ?? "");
+        SearchSettings.SizeSliderHigherValue = Convert.ToDouble(reader.ReadLine() ?? "");
+        SearchSettings.DowntimeLowerValue = Convert.ToDouble(reader.ReadLine() ?? "");
+        SearchSettings.DowntimeHigherValue = Convert.ToDouble(reader.ReadLine() ?? "");
+        SearchSettings.AreaSliderLowerValue = Convert.ToDouble(reader.ReadLine() ?? "");
+        SearchSettings.AreaSliderHigherValue = Convert.ToDouble(reader.ReadLine() ?? "");
+
+
+        SearchSettings.MinPrKvm = Convert.ToInt32(reader.ReadLine() ?? "");
+        SearchSettings.MaxPrKvm = Convert.ToInt32(reader.ReadLine() ?? "");
+        SearchSettings.MinGroundSize = Convert.ToInt32(reader.ReadLine() ?? "");
+        SearchSettings.MaxGroundSize = Convert.ToInt32(reader.ReadLine() ?? "");
+        SearchSettings.MinYearBuilt = Convert.ToInt32(reader.ReadLine() ?? "");
+        SearchSettings.MaxYearBuilt = Convert.ToInt32(reader.ReadLine() ?? "");
+        SearchSettings.MinRoomCount = Convert.ToInt32(reader.ReadLine() ?? "");
+        SearchSettings.MaxRoomCount = Convert.ToInt32(reader.ReadLine() ?? "");
+
+        SearchSettings.Sold = Convert.ToBoolean(reader.ReadLine() ?? "");
+        SearchSettings.ForSale = Convert.ToBoolean(reader.ReadLine() ?? "");
+        SearchSettings.SameRoad = Convert.ToBoolean(reader.ReadLine() ?? "");
+        SearchSettings.SameZipCode = Convert.ToBoolean(reader.ReadLine() ?? "");
+
+
+      }
+    }
+  }
 }
